09573af312c4c740ab65734c116f8e047a6a2bfe
joaonayres <joaonayres@gmail.com>
2026-03-04 17:01:51 -0400

    Refactor product data and update image paths

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const produtos = [
     nome: 'Sapato Masculino',
     descricao: 'Sapato infantil feito a mão.',
     categoria: 'Sapato Infantil',
-    thumb: 'img/kit_sapato_masculino.Webp'
+    thumb: '../img/Kit_sapato_masculino.Webp'
   },
   {
     nome: 'Sapato Feminino',
@@ -229,6 +229,7 @@ document.getElementById("menuContato").addEventListener("click", function(e) {
   );
 
 });
+
 
 
 
